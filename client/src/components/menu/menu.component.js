8606588bbadb965af8120387d8b298ec0878d611
Marek <marek.szczepanski1@onet.pl>
2020-05-14 15:41:18 +0200

    add menu conception

--- client/src/components/menu/menu.component.js
+++ client/src/components/menu/menu.component.js
@@ -2,6 +2,8 @@ import React from "react";
 import "../../styles/styles.scss";
 /* import Menu from "../../assets/jpg/menu.jpg"; */
 import animation from "../../modules/animation";
+import { ReactComponent as ArrowLeft } from "../../assets/svg/sanbiagio-ostroda-left-arrow.svg";
+import { ReactComponent as ArrowRight } from "../../assets/svg/sanbiagio-ostroda-right-arrow.svg";
 
 class Maenu extends React.Component {
   render() {
@@ -9,11 +11,71 @@ class Maenu extends React.Component {
       <section className="menuSection">
         <h2>Menu</h2>
         <div className="sliderWrap">
-          <div className="breakfast">
+          <div className="slideWrap">
             <h4>Śniadanie</h4>
-            <h6>Od godz. 10:00 do 12:00</h6>
+            <h6 className="breakfastH6">Od godz. 10:00 do 12:00</h6>
+            <h6>Jajecznica z 4 jaj (podajemy z pieczywem i masłem)</h6>
+            <p className="menuP">
+              na maśle
+              <span className="menuDots">
+                ............................................
+              </span>
+              <span className="menuPrice">11,00 zł</span>
+            </p>
+            <p className="menuP">
+              z szynką lub boczkiem
+              <span className="menuDots">
+                ...................................
+              </span>
+              <span className="menuPrice">13,00 zł</span>
+            </p>
+            <h6 className="menuH6MarginTop">Naleśniki - 2 sztuki</h6>
+            <p className="menuP">
+              z serem
+              <span className="menuDots">
+                .............................................
+              </span>
+              <span className="menuPrice">12,00 zł</span>
+            </p>
+            <p className="menuP">
+              z dżemem
+              <span className="menuDots">
+                ...........................................
+              </span>
+              <span className="menuPrice">12,00 zł</span>
+            </p>
+            <p className="menuP">
+              z owocami
+              <span className="menuDots">
+                ...........................................
+              </span>
+              <span className="menuPrice">15,00 zł</span>
+            </p>
+            <h6 className="menuH6MarginTop">
+              62. Frittata (włoski omlet z brokułami)
+              <span className="menuDots">.........................</span>
+              <span className="menuH6Price">16,00 zł</span>
+            </h6>
+            <h6 className="menuH6MarginTop">
+              63. Frittata (włoski omlet z papryką, cebulą i pieczarkami)
+              <span className="menuDots">............</span>
+              <span className="menuH6Price">17,00 zł</span>
+            </h6>
           </div>
         </div>
+        <p className="menuP">
+          <span className="menuDots"></span>
+        </p>
+        <ArrowLeft
+          id="arrowLeft"
+          className="sliderArrow"
+          onClick={this.handleClick}
+        />
+        <ArrowRight
+          id="arrowRight"
+          className="sliderArrow"
+          onClick={this.handleClick}
+        />
       </section>
     );
   }
